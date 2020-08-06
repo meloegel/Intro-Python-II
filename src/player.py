1fe3d732b4249483c9ea9209833e81b39936eaa4
meloegel <meloegel@gmail.com>
2020-08-06 18:00:50 -0400

    mvp and stretch

--- src/player.py
+++ src/player.py
@@ -2,8 +2,18 @@
 # currently.
 
 
-class Player:
+class Player():
     def __init__(self, name, location):
         self.name = name
         self.location = location
+        self.backpack = []
+
+    def __str__(self):
+        return f'Player: {self.name}, {self.location}'
+
+    def take(self, item):
+        self.backpack.append(item)
+
+    def drop(self, item):
+       del self.backpack[item]
     
--- src/room.py
+++ src/room.py
@@ -1,7 +1,18 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from item import Item
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, item):
         self.name = name
         self.description = description
+        self.item =[item]
+
+    def __str__(self):
+        return f'Room: {self.name}, Description: {self.description}'
+
+    def remove_item(self, item):
+        del self.item[item]
+
+    def add_item(self, item):
+        self.item.append(item)
